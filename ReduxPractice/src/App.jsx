374b0cc6eb4a64edacfa60a469807f73be33dc4a
Hayan Khan <hayankhanmehsood@gmail.com>
2024-06-27 23:49:32 +0500

    react Redux

--- ReduxPractice/src/App.jsx
+++ ReduxPractice/src/App.jsx
@@ -2,12 +2,20 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import AddTodo from './components/AddTodo'
+import Todos from './components/Todos'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-   <div className='h-screen w-screen bg-black'>
+   <div className='h-screen w-screen flex flex-col justify-center items-center bg-black'>
+  
+  <h1 className='text-4xl text-white text-center font-bold'>React Redux</h1>
+ <div className='w-full flex flex-col items-center'> <AddTodo/>
+ </div>
+<div className='w-full px-4'>
+<Todos  />
+</div>
 
    </div>
   )
